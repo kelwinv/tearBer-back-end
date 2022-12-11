7578686ac5dd13c4bb2a95e0e76b5bb0727c5ad0
kelwinv <kelwin08@gmail.com>
2022-12-11 17:39:06 -0300

    init project

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('AppController', () => {
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [],
+      providers: [],
+    }).compile();
+
+    // appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      // expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
